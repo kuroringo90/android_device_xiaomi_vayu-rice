09a129af84d605ee8cc03d072c04b700b9de4ad6
VEN0M98 <bt98.bt7@gmail.com>
2022-12-26 00:30:56 +0000

    vayu: Bananadroid BringUp

--- banana_vayu.mk
+++ banana_vayu.mk
@@ -8,21 +8,17 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit some common banana stuff.
+$(call inherit-product, vendor/banana/config/common.mk)
 
 # Inherit from vayu device
 $(call inherit-product, device/xiaomi/vayu/device.mk)
 
 # Rom flags
-WITH_GMS := true
-PALYRIM_BUILD_TYPE := UNOFFICIAL
-PALYRIM_MAINTAINER := Theperryteam
-TARGET_FACE_UNLOCK_SUPPORTED := true
-TARGET_SUPPORTS_QUICK_TAP := true
-TARGET_BOOT_ANIMATION_RES := 1080
+BANANA_MAINTAINER := PerryTheTeam
+BANANA_BUILD_TYPE := UNOFFICIAL
 
-PRODUCT_NAME := lineage_vayu
+PRODUCT_NAME := banana_vayu
 PRODUCT_DEVICE := vayu
 PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_BRAND := POCO
