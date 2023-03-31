fdccc0afa23579f42af3fe2cebacc755f50f3fff
kuroringo90 <ak4700@gmx.com>
2023-03-31 20:52:53 +0200

    vayu: update

--- libinit/init_xiaomi_vayu.cpp
+++ libinit/init_xiaomi_vayu.cpp
@@ -8,6 +8,25 @@
 #include <libinit_variant.h>
 
 #include "vendor_init.h"
+#include <cstdlib>
+#define _REALLY_INCLUDE_SYS__SYSTEM_PROPERTIES_H_
+#include <sys/_system_properties.h>
+#include <sys/sysinfo.h>
+#include <android-base/properties.h>
+#include "property_service.h"
+
+using android::base::GetProperty;
+using std::string;
+
+void property_override(string prop, string value)
+{
+    auto pi = (prop_info*) __system_property_find(prop.c_str());
+
+    if (pi != nullptr)
+        __system_property_update(pi, value.c_str(), value.size());
+    else
+        __system_property_add(prop.c_str(), prop.size(), value.c_str(), value.size());
+}
 
 static const variant_info_t vayu_global_info = {
     .hwc_value = "GLOBAL",
@@ -60,4 +79,8 @@ static const std::vector<variant_info_t> variants = {
 void vendor_load_properties() {
     set_dalvik_heap();
     search_variant(variants);
+
+    // Misc
+    property_override("ro.rice.chipset", "SDM860");
+
 }
