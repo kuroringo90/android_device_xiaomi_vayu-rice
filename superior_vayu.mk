e7ebac0eac0260b3b1afa9391d9c19f20740e185
kuroringo90 <ak4700@gmx.com>
2023-09-01 12:58:31 +0000

    vayu: fix typo

--- superior_vayu.mk
+++ superior_vayu.mk
@@ -9,7 +9,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit some common SuperiorExtendedOS  stuff.
-$(call inherit-product, vendor/superior/config/common_full_phone.mk)
+$(call inherit-product, vendor/superior/config/common.mk)
 
 # Inherit from vayu device
 $(call inherit-product, device/xiaomi/vayu/device.mk)
@@ -31,6 +31,7 @@ BUILD_WITH_GAPPS := true
 USE_QUICKPIC := false
 USE_DUCKDUCKGO := false
 TARGET_FACE_UNLOCK_SUPPORTED := true
+TARGET_BOOT_ANIMATION_RES := 1080
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
