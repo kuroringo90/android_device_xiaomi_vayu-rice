df2b24030ead397f5a70dbc345b76781ab87199b
kuroringo90 <ak4700@gmx.com>
2023-09-01 12:23:56 +0000

    vayu: Superior Extended Bringup

--- superior_vayu.mk
+++ superior_vayu.mk
@@ -8,17 +8,29 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit some common SuperiorExtendedOS  stuff.
+$(call inherit-product, vendor/superior/config/common_full_phone.mk)
 
 # Inherit from vayu device
 $(call inherit-product, device/xiaomi/vayu/device.mk)
 
-PRODUCT_NAME := lineage_vayu
+PRODUCT_NAME := superior_vayu
 PRODUCT_DEVICE := vayu
 PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_BRAND := POCO
 PRODUCT_MODEL := POCO X3 Pro
+
+TARGET_INCLUDE_MATLOG := false
+TARGET_INCLUDE_PIXEL_CHARGER := true
+USE_MOTO_CALCULATOR := false
+TARGET_SUPPORTS_BLUR := true
+USE_MOTO_CLOCK := false
+SYSTEM_OPTIMIZE_JAVA := true
+SYSTEMUI_OPTIMIZE_JAVA := true
+BUILD_WITH_GAPPS := true
+USE_QUICKPIC := false
+USE_DUCKDUCKGO := false
+TARGET_FACE_UNLOCK_SUPPORTED := true
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
