a377513f10d89a900560c022109ff43b3d298038
kuroringo90 <ak4700@gmx.com>
2024-01-14 10:28:12 +0000

    vayu: Welcome to CalyxOS

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,9 +5,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/superior_vayu.mk
+    $(LOCAL_DIR)/calyx_vayu.mk
 
 COMMON_LUNCH_CHOICES := \
-    superior_vayu-user \
-    superior_vayu-userdebug \
-    superior_vayu-eng
+    calyx_vayu-user \
+    calyx_vayu-userdebug \
+    calyx_vayu-eng
---
+++ calyx_vayu.mk
@@ -0,0 +1,28 @@
+#
+# Copyright (C) 2021 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base_telephony.mk)
+
+# Inherit some common CalyxOS  stuff.
+$(call inherit-product, vendor/calyx/config/common.mk)
+
+# Inherit from vayu device
+$(call inherit-product, device/xiaomi/vayu/device.mk)
+
+PRODUCT_NAME := calyx_vayu
+PRODUCT_DEVICE := vayu
+PRODUCT_MANUFACTURER := Xiaomi
+PRODUCT_BRAND := POCO
+PRODUCT_MODEL := POCO X3 Pro
+INLINE_KERNEL_BUILDING := true
+
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRIVATE_BUILD_DESC="vayu_global-user 13 RKQ1.200826.002 V14.0.1.0.TJUMIXM release-keys"
+
+BUILD_FINGERPRINT := POCO/vayu_global/vayu:13/RKQ1.200826.002/V14.0.1.0.TJUMIXM:user/release-keys
