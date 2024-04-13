4a90210b63066a1211dd6d06d5d7018bcd094d3a
kuroringo90 <ak4700@gmx.com>
2024-04-13 09:52:30 +0200

    vayu: update rising bringup

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,9 +5,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/rising_vayu.mk
+    $(LOCAL_DIR)/lineage_vayu.mk
 
 COMMON_LUNCH_CHOICES := \
-    rising_vayu-user \
-    rising_vayu-userdebug \
-    rising_vayu-eng
+    lineage_vayu-user \
+    lineage_vayu-userdebug \
+    lineage_vayu-eng
