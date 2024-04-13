83ed4b5a466afc39953b5e8848e11c66c7471299
kuroringo90 <ak4700@gmx.com>
2024-04-13 13:41:17 +0200

    !fixup vayu: rising new bringup

--- lineage_vayu.mk
+++ lineage_vayu.mk
@@ -9,7 +9,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit some common Rising stuff.
-$(call inherit-product, vendor/rising/config/rising.mk)
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
 # Inherit from vayu device
 $(call inherit-product, device/xiaomi/vayu/device.mk)
