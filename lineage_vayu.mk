1829c61b7a11c402ae30c08996ec3a76b4b07d2a
kuroringo90 <ak4700@gmx.com>
2024-05-30 01:43:03 +0200

    vayu: update maintaner flag

--- lineage_vayu.mk
+++ lineage_vayu.mk
@@ -16,8 +16,9 @@ $(call inherit-product, device/xiaomi/vayu/device.mk)
 
 # Device Identifiers
 PRODUCT_BUILD_PROP_OVERRIDES += \
-   RISING_MAINTAINER="ste0090" \
    RISING_CHIPSET="SDM860"    
+
+RISING_MAINTAINER=ste0090
 
 WITH_GMS := true
 TARGET_CORE_GMS := true
