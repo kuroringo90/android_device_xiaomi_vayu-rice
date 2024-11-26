d0005549db64de42644b7dc9c9a119d52d33b0c1
minaripenguin37 <alexfinhart@gmail.com>
2024-11-26 03:55:20 +0100

    vayu: Build Aperture camera

--- device.mk
+++ device.mk
@@ -435,8 +435,8 @@ PRODUCT_COPY_FILES += \
     frameworks/av/media/libstagefright/data/media_codecs_google_c2_audio.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_google_c2_audio.xml \
     frameworks/av/media/libstagefright/data/media_codecs_google_c2_video.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_google_c2_video.xml
 
-# MiuiCamera
--include vendor/xiaomi/miuicamera/config.mk
+PRODUCT_PACKAGES += \
+    Aperture
 
 # Network
 PRODUCT_PACKAGES += \
