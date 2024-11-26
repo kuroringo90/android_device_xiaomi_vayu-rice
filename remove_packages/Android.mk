4f06cb044ed7380ef02a6b6b6d9eaf0dce4645ee
kuroringo90 <ak4700@gmx.com>
2024-11-26 04:12:38 +0100

    vayu: update remove gapps

--- remove_packages/Android.mk
+++ remove_packages/Android.mk
@@ -4,28 +4,7 @@ include $(CLEAR_VARS)
 LOCAL_MODULE := RemovePackagesVayu
 LOCAL_MODULE_CLASS := APPS
 LOCAL_MODULE_TAGS := optional
-LOCAL_OVERRIDES_PACKAGES := \
-    Accord \
-    Chrome64 \
-    Chrome \
-    Chrome-Stub \
-    ConnMO \
-    Drive \
-    DCMO \
-    FilesGoogle \
-    FilesPrebuilt  \
-    Gramophone \
-    Maestro \
-    Maps \
-    PersonalSafety \
-    RecorderPrebuilt \
-    SafetyHubPrebuilt \
-    Symphonica \
-    Tycho \
-    VoiceAccessPrebuilt \
-    VZWAPNLib \
-    YouTube
-
+LOCAL_OVERRIDES_PACKAGES := Updater LocationHistoryPrebuilt PrebuiltGoogleTelemetryTvp WallpaperEmojiPrebuilt-v470 WallpaperEffect GoogleFeedback GooglePrintRecommendationService AmbientStreaming talkback
 LOCAL_UNINSTALLABLE_MODULE := true
 LOCAL_CERTIFICATE := PRESIGNED
 LOCAL_SRC_FILES := /dev/null
