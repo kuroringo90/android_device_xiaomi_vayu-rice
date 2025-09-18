b24e832c02c2b8fb2ae629ac8f5bc9eb77c5c3fd
kuroringo90 <ak4700@gmx.com>
2025-09-18 21:43:19 +0200

    vayu: axion 16

--- lineage_vayu.mk
+++ lineage_vayu.mk
@@ -15,6 +15,25 @@ $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 # Inherit from vayu device
 $(call inherit-product, device/xiaomi/vayu/device.mk)
 
+# Axion stuff
+AXION_MAINTAINER := STE
+AXION_PROCESSOR := Snapdragon_870
+
+# Axion CPU Flags
+AXION_CPU_SMALL_CORES := 0,1,2,3
+AXION_CPU_BIG_CORES := 4,5,6,7
+
+# CPUsets configuration
+AXION_ALL_CORES := 0-7
+AXION_CPU_BG := 0-3
+AXION_CPU_FG := 0-6
+AXION_CPU_LIMIT_BG := 0-1
+AXION_CPU_LIMIT_UI := 4-7
+AXION_CPU_DISPLAY := 4-7
+# Camera
+AXION_CAMERA_REAR_INFO := 50
+AXION_CAMERA_FRONT_INFO := 8
+
 PRODUCT_NAME := lineage_vayu
 PRODUCT_DEVICE := vayu
 PRODUCT_MANUFACTURER := Xiaomi
